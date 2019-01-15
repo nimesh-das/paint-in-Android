2bde9618ec8a5b85c06328b5cb59835c5bb1419d
Nimesh Das <ndas@opentext.com>
2019-01-14 21:19:54 -0500

    Remove directories

--- monopoly/observer.cc
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "observer.h"
-using namespace std;
-
-Observer::~Observer() {}
-
--- monopoly/unownable.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "unownable.h"
-using namespace std;
-
-Unownable::Unownable(): Cell() { }
-
-Unownable::Unownable(string cellName, int indexOfProperty, Board *leboard): Cell(cellName, indexOfProperty, leboard) { }
-
-Unownable::~Unownable() { }
